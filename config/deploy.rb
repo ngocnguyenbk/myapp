288b17ea2adf5b7b454426b831ce7b5fb3b29e8d
Nguyen Ngoc <nguyenvanngoc1996@gmail.com>
2020-10-03 11:02:06 +0700

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :pty, true
 set :linked_files, %w(config/database.yml config/secrets.yml)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 5
-set :rvm_type, :system
+set :rvm_type, :user
 set :passenger_restart_with_touch, true
 
 set :puma_rackup, -> {File.join(current_path, "config.ru")}
