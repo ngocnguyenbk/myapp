250bcef394faf95f99f47c5e1ef7415022edef7c
nv-ngoc <nv-ngoc@ruby-dev.jp>
2020-10-23 14:51:05 +0700

    11023: Add floors, rooms.

---
+++ app/javascript/component_floors/tableFloors.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RowFloor
+      :floor="floor" v-for="(floor, index) in floors" :key="index"
+    />
+  </div>
+</template>
+
+<script>
+import RowFloor from './rowFloor.vue'
+
+export default {
+  components: {
+    RowFloor
+  },
+  props: {
+    floors: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
