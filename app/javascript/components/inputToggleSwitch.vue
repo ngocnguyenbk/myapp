29abf83ea0a409e97f4949543099352d1f80edcf
Nguyen Van Ngoc <nv-ngoc@ruby-dev.jp>
2020-11-27 16:32:59 +0700

    30: Confirm before deleting.

--- app/javascript/components/inputToggleSwitch.vue
+++ app/javascript/components/inputToggleSwitch.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="form-group row">
-    <label :for="inputId" class="col-lg-2 col-form-label">{{lableText}}</label>
-    <div class="col-lg-10">
+    <div class="col-lg-2">
       <label class="switch">
         <input type="checkbox" v-model="checked">
         <span class="slider round"></span>
       </label>
     </div>
+    <label :for="inputId" class="col-lg-2 col-form-label">{{lableText}}</label>
   </div>
 </template>
 
@@ -32,7 +32,7 @@ export default {
   },
   watch: {
     checked: function(val) {
-      return this.$emit('input', val)
+      return this.$emit('checked', val)
     },
     value: function(val) {
       return this.checked = val
