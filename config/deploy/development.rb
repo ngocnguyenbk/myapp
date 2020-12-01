3b8b7736e0673c17ccdc894d63d93ab90f438c88
Nguyen Van Ngoc <nv-ngoc@ruby-dev.jp>
2020-12-01 11:09:00 +0700

    Fix css-loader

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -5,7 +5,7 @@ set :deploy_to, "/var/www/myapp"
 
 set :branch, :develop
 
-server "52.91.120.154", user: fetch(:user), roles: %w[app db batch]
+server "18.138.254.4", user: fetch(:user), roles: %w[app db batch]
 
 set :ssh_options,
     keys: %w[.ssh/deploy_cloudwatch_details.pem],
