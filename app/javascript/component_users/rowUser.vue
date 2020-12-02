b9248713141a88039b5092fdfe1ccc972017404d
Nguyen Ngoc <nguyenvanngoc1996@gmail.com>
2020-12-02 23:16:55 +0700

    39: Edit/Delete User button

--- app/javascript/component_users/rowUser.vue
+++ app/javascript/component_users/rowUser.vue
@@ -9,7 +9,19 @@
       <td>{{user.birth_day}}</td>
       <td>{{user.created_date}}</td>
       <td class="text-center">
-        <div :class="['dot dot-sm ml-auto', status_user]"></div>
+        <i class="fas fa-edit text-primary pointer mr-2"
+           data-toggle="modal"
+           data-target="#editUser"
+           @click="setCurrentUser"
+        />
+        <i :class="['far fa-stop-circle text-danger ml-2', user.status_active ? 'pointer' : 'pointer_disable']"
+           data-toggle="modal"
+           data-target="#deleteUser"
+           @click="setCurrentUser"
+        />
+      </td>
+      <td class="text-center">
+        <div :class="['dot dot-sm ml-auto align-middle', status_user]"></div>
       </td>
     </tr>
   </tbody>
@@ -44,4 +56,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .pointer {
+    cursor: pointer;
+  }
+
+  .pointer_disable {
+    pointer-events: none;
+    color: gray !important;
+  }
 </style>
